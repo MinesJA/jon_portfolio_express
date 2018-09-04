dc331fae481a1e9b3d2283d9c51d56898ee058a8
Jonathan Mines <minesja@gmail.com>
2018-09-04 16:55:26 -0400

    Changing front page

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var rssUrl = "https://medium.com/feed/@jonathanmines"
 
 
 router.get('/', function(req, res, next) {
+  var skills = ["Ruby", "Rails", "Rack", "Sinatra", "ActiveRecord", "Javascript", "Node", "Express", "React", "Redux", "Python", "Git", "SQL", "PostGres", "SQLite", "JWT", "Bcrypt", "Jade"]
   res.render('index');
 });
 
