be842b8a6051b13485eeedfbd0600e55fc85eafd
Jonathan Mines <minesja@gmail.com>
2018-09-07 11:58:17 -0400

    Pushing to heroku

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/blog', indexRouter);
 app.use('/resume', indexRouter);
 app.use('/projects', indexRouter);
 app.use('/contact', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,14 @@
-var feed = require("feed-read-parser");
 var express = require('express');
 var router = express.Router();
 var rssUrl = "https://medium.com/feed/@jonathanmines"
 
 
 router.get('/', function(req, res, next) {
-  var skills = ["Ruby", "Rails", "Rack", "Sinatra", "ActiveRecord", "Javascript", "Node", "Express", "React", "Redux", "Python", "Git", "SQL", "PostGres", "SQLite", "JWT", "Bcrypt", "Jade"]
   res.render('index');
 });
 
 router.get('/resume', function(req, res, next){
   res.render('resume');
-})
-
-router.get('/blog', function(req, res, next){
-  res.render('blog', {rss: "hey"});
 })
 
 router.get('/projects', function(req, res, next){
